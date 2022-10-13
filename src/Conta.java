e15ba9b1676e41b75cbeba2fcf9dd89427b80507
Daniel <danielaires95@gmail.com>
2022-10-13 15:54:13 -0300

    Projeto concluido.

---
+++ src/Conta.java
@@ -0,0 +1,9 @@
+public class Conta {
+
+   public int numero;
+   public String texto;
+   public String nomeCliente;
+   public double saldo;
+
+
+}
--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        System.out.println("Hello Word!!");
+       // System.out.println("Hello Word!!");
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite seu nome:");
+        String nomeCliente = entrada.next();
+        System.out.println("Digite a agencia:");
+        String texto = entrada.next();
+        System.out.println("Digite a conta:");
+        int numero = entrada.nextInt();
+        System.out.println("Digite o saldo:");
+        double saldo = entrada.nextDouble();
+
+        System.out.println("Ola "+nomeCliente+" obrigado por criar uma conta em nosso banco, sua agencia e "+texto+ " conta: "+numero+ " e seu saldo: " +saldo+" ja esta disponivel para saque.");
+
     }
 }
